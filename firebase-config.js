58f03c0ac8b96d41241e979b99e86dbd1e582478
blumark24 <blumark24@gmail.com>
2025-10-28 19:33:43 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ9iNZn0uxdkDSjKV6IlXGC5FW5-lxgZI",
+  authDomain: "smart-hsr-manager.firebaseapp.com",
+  projectId: "smart-hsr-manager",
+  storageBucket: "smart-hsr-manager.firebasestorage.app",
+  messagingSenderId: "38965508031",
+  appId: "1:38965508031:web:6fd0b6c6b0b63fa513930a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
