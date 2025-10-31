ad10f4a51f6511834c1805b98e5cd62e75bea3e1
blumark24 <blumark24@gmail.com>
2025-10-31 18:44:13 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,19 @@
+// =============================
+// Firebase Configuration File
+// =============================
+
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBQ9iNZn0uxdkDSjKV6IlXGC5FW5-lxgZI",
   authDomain: "smart-hsr-manager.firebaseapp.com",
   projectId: "smart-hsr-manager",
-  storageBucket: "smart-hsr-manager.firebasestorage.app",
+  storageBucket: "smart-hsr-manager.appspot.com", // ✅ التصحيح هنا
   messagingSenderId: "38965508031",
   appId: "1:38965508031:web:6fd0b6c6b0b63fa513930a"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
