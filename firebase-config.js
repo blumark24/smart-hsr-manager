5727458da4c1a2b0e89c0239dd235c87d335adde
blumark24 <blumark24@gmail.com>
2025-11-12 12:55:15 +0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// إعدادات Firebase الخاصة بك
-const firebaseConfig = {
-  apiKey: "AIzaSyBQ9iNZn0uxdkDSjKV6IlXGC5FW5-lxgZI",
-  authDomain: "smart-hsr-manager.firebaseapp.com",
-  projectId: "smart-hsr-manager",
-  storageBucket: "smart-hsr-manager-storage.appspot.com", // ✅ هذا مهم جدًا
-  messagingSenderId: "38965508031",
-  appId: "1:38965508031:web:6fd0b6c6b0b63fa513930a"
-};
